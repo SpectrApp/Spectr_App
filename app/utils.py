f786cad6f0cba5d00b4c2bc672d2d14bf626305b
dm1trykrylov <231014d@gmail.com>
2023-08-26 23:44:27 +0400

    Add try-except for request to dadata

--- app/utils.py
+++ app/utils.py
@@ -6,9 +6,12 @@ def get_info_by_address(address : str):
   token = app.config['DADATA_API_KEY']
   secret = app.config['DADATA_SECRET_KEY']
   dadata = Dadata(token, secret)
-  result = dadata.clean("address", address)
+  try:
+    result = dadata.clean("address", address)
+  except:
+    result = None
   
   address_data = result
-  if ('qc_geo' not in address_data or address_data['qc_geo'] > 1):
+  if (address_data is None or 'qc_geo' not in address_data or address_data['qc_geo'] > 1):
     return None
   return address_data
