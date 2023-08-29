65ec60b918892d7f27698fa169462edc97335263
dm1trykrylov <231014d@gmail.com>
2023-08-29 15:55:56 +0400

    Remove outdated templates and endpoints

--- app/routes.py
+++ app/routes.py
@@ -22,103 +22,6 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-
-@app.route('/index', methods=['GET', 'POST'])
-@login_required
-def index():
-    form = PostForm()
-    if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
-        db.session.add(post)
-        db.session.commit()
-        flash('Your post is now live!')
-        return redirect(url_for('index'))
-    posts = current_user.followed_posts().all()
-    return render_template("index.html", title='Home Page', form=form,
-                           posts=posts)
-
-
-@app.route('/help')
-def help():
-    form = HelpForm()
-    with open('app/help.txt') as f:
-        help_message = f.readlines()
-    return render_template("help.html", title='Help', form=form, help_message=help_message)
-
-
-@app.route('/user/<username>')
-@login_required
-def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    posts = user.posts.order_by(Post.timestamp.desc()).all()
-    form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
-
-
-@app.route('/edit_profile', methods=['GET', 'POST'])
-@login_required
-def edit_profile():
-    form = EditProfileForm()
-    if form.validate_on_submit():
-        current_user.username = form.username.data
-        current_user.bio = form.bio.data
-        db.session.commit()
-        flash('Your changes have been saved.')
-        return redirect(url_for('index'))
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.bio.data = current_user.bio
-    return render_template('edit_profile.html', title='Edit Profile',
-                           form=form)
-
-
-@app.route('/delete_profile', methods=['GET', 'POST'])
-@login_required
-def delete_profile():
-    form = DeleteProfileForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=current_user.username).first()
-        db.session.delete(user)
-        db.session.commit()
-        flash('Your changes have been saved. Bye.')
-        return redirect(url_for('index'))
-    return render_template('delete_profile.html', title='DELETE Profile',
-                           form=form)
-
-
-@app.route('/explore')
-@login_required
-def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts)
-
-
-@app.route('/send_message/<recipient>', methods=['GET', 'POST'])
-@login_required
-def send_message(recipient):
-    user = User.query.filter_by(username=recipient).first_or_404()
-    form = MessageForm()
-    if form.validate_on_submit():
-        msg = Message(author=current_user, recipient=user,
-                      body=form.message.data)
-        db.session.add(msg)
-        db.session.commit()
-        flash('Your message has been sent.')
-        return redirect(url_for('user', username=recipient))
-    return render_template('send_message.html', title='Send Message',
-                           form=form, recipient=recipient)
-
-
-@app.route('/messages')
-@login_required
-def messages():
-    current_user.last_message_read_time = datetime.utcnow()
-    db.session.commit()
-    messages = current_user.messages_received.order_by(
-        Message.timestamp.desc())
-    return render_template('messages.html', messages=messages)
-
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -155,65 +58,12 @@ def logout():
     return redirect(url_for('dashboard'))
 
 
-@app.route('/follow/<username>', methods=['POST'])
-@login_required
-def follow(username):
-    form = EmptyForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=username).first()
-        if user is None:
-            flash('User {} not found.'.format(username))
-            return redirect(url_for('index'))
-        if user == current_user:
-            flash('You cannot follow yourself!')
-            return redirect(url_for('user', username=username))
-        current_user.follow(user)
-        db.session.commit()
-        flash('You are following {}!'.format(username))
-        return redirect(url_for('user', username=username))
-    else:
-        return redirect(url_for('index'))
-
-
-@app.route('/reply_to/<post_id>', methods=['POST'])
-@login_required
-def reply(post_id):
-    form = ReplyForm()
-    if form.validate_on_submit():
-        comment = Comment(body=form.post.data,
-                          author=current_user, parent_id=post_id)
-        db.session.add(comment)
-        db.session.commit()
-        return redirect(url_for('index'))
-    else:
-        return redirect(url_for('index'))
-
-
-@app.route('/unfollow/<username>', methods=['POST'])
-@login_required
-def unfollow(username):
-    form = EmptyForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=username).first()
-        if user is None:
-            flash('User {} not found.'.format(username))
-            return redirect(url_for('index'))
-        if user == current_user:
-            flash('You cannot unfollow yourself!')
-            return redirect(url_for('user', username=username))
-        current_user.unfollow(user)
-        db.session.commit()
-        flash('You are not following {}.'.format(username))
-        return redirect(url_for('user', username=username))
-    else:
-        return redirect(url_for('index'))
-
-
 DATASET_PATH = None
 DATASET_NAME = "example"
 
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @app.route('/dashboard', methods=['POST', 'GET'])
 @login_required
 def dashboard():
